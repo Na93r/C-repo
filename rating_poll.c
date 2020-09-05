d98e30fea8c3d8f0ebbe1f22b12f67635b67dbf6
Nasser Malam <55814389+Na93r@users.noreply.github.com>
2020-09-05 15:54:10 +0200

    Update rating_poll.c
    add comments to the program

--- rating_poll.c
+++ rating_poll.c
@@ -1,4 +1,9 @@
-// a program that analyze a rating poll
+// this program solve and analayze this porblem 
+
+/*
+forty student were asked to rate the quality of the food in the student cafeteria on a scale of 1 to 10 (1 means awful and 10 means exellent).
+place the 40 responses in an integer array and summarize the result of the poll.
+*/
 #include<stdio.h>
 #define RESPONSES_SIZE 40
 #define FREQUENCY_SIZE 11
